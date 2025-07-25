c5d477dad8af5e1d34465e0cbc63c641feebb9d0
Laxmibabar <lakshmibabar9@gmail.com>
2025-07-25 16:07:37 +0530

    frontend changes for userprofile added 2

--- frontend/src/components/UserProfile/UserMetaCard.tsx
+++ frontend/src/components/UserProfile/UserMetaCard.tsx
@@ -17,7 +17,7 @@ export default function UserMetaCard({ user }: UserMetaCardProps) {
   };
   const fullName = user.full_name || user.email;
   const city = user.city || "-";
-  const country = user.country || "-";
+  // const country = user.country || "-";
   return (
     <>
       <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
